f5460151d0fa45705d936b82c484bcf5147ea9f1
Bryant Gossett <5419632+Bgosse1@users.noreply.github.com>
2019-03-02 10:06:52 -0500

    refactored navbar to class

--- clicky-game/src/components/Footer/index.js
+++ clicky-game/src/components/Footer/index.js
@@ -1,19 +1,6 @@
 import React, {Component} from "react";
 import "./style.css";
 
-// function Footer() {
-//   return (
-//     <footer className="footer">
-//         <div className="bottom">
-//             Clicky Game
-//         </div>
-//     </footer>
-//   );
-// }
-
-// export default Footer;
-
-
 class Footer extends Component {
   render() {
     return (
--- clicky-game/src/components/Navbar/index.js
+++ clicky-game/src/components/Navbar/index.js
@@ -1,21 +1,23 @@
-import React from "react";
+import React, {Component} from "react";
 import "./style.css";
 import { Nav, Navbar } from "react-bootstrap";
 
-function NavBar(props) {
+class NavBar extends Component {
+  render() {
     return (
       <Nav justify defaultActiveKey="/" className="nav-bar">
         <Nav.Item>
           <Nav.Link href="/" className="nav-link">Clicky Game</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Navbar.Text>{props.message}</Navbar.Text>
+          <Navbar.Text>{this.props.message}</Navbar.Text>
         </Nav.Item>
         <Nav.Item>
-          <Navbar.Text>Score: {props.correctGusses} | Top Score: {props.topScore}</Navbar.Text>
+          <Navbar.Text>Score: {this.props.correctGusses} | Top Score: {this.props.topScore}</Navbar.Text>
         </Nav.Item>
       </Nav>
     );
-};
+  }
+}
 
-export default NavBar;
+export default NavBar
