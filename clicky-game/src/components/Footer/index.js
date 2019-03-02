652e3d4798fdec1f4499895ae0c343291e433ec1
Bryant Gossett <5419632+Bgosse1@users.noreply.github.com>
2019-03-02 10:03:41 -0500

    refactored footer to class

--- clicky-game/src/components/Footer/index.js
+++ clicky-game/src/components/Footer/index.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {Component} from "react";
 import "./style.css";
 
-function Footer() {
-  return (
-    <footer className="footer">
-        <div className="bottom">
-            Clicky Game
-        </div>
-    </footer>
-  );
+// function Footer() {
+//   return (
+//     <footer className="footer">
+//         <div className="bottom">
+//             Clicky Game
+//         </div>
+//     </footer>
+//   );
+// }
+
+// export default Footer;
+
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+          <div className="bottom">
+              Clicky Game
+          </div>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
